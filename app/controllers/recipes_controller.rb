6f0d418ede90b42c8b4da70dd422fddfa1595316
Will Hodgson <will.hodgson@current-rms.com>
2019-11-04 14:59:50 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,6 +16,6 @@ class RecipesController < ApplicationController
 
   private
     def recipe_params
-      params.require(:recipe).permit
+      params.require(:recipe).permit(:title, )
     end
 end
